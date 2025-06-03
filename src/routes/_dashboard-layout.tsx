9eb5d31a6c56c3b9c9a2a0d26ee443ba68a97bd8
zazawowow <zazawowow@icloud.com>
2025-06-03 12:15:29 +0100

    fix: update heading levels and add h3 styles for improved text hierarchy

--- src/routes/_dashboard-layout.tsx
+++ src/routes/_dashboard-layout.tsx
@@ -109,9 +109,9 @@ function DashboardLayout() {
 							<div className="lg:space-y-2">
 								{dashboardNavigation.map((section) => (
 									<div key={section.title}>
-										<h2 className="text-xl font-heading bg-tertiary-black text-white px-4 py-2  mb-0 lg:mb-2">
+										<h3 className="font-heading bg-tertiary-black text-white px-4 py-2 mb-0 lg:mb-2">
 											{section.title}
-										</h2>
+										</h3>
 										<nav className="space-y-2 p-4 lg:p-0 text-xl lg:text-base">
 											{section.title === 'SALES' && (
 												<Link
