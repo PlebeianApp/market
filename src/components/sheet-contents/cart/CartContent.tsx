2cf65318ef060af50943087e0acb6d7d858559cd
zazawowow <zazawowow@icloud.com>
2025-08-07 16:54:21 -0500

    ui : removing some spacing errors

--- src/components/sheet-contents/cart/CartContent.tsx
+++ src/components/sheet-contents/cart/CartContent.tsx
@@ -122,7 +122,7 @@ export function CartContent({ className = '' }: { className?: string }) {
 	}
 
 	return (
-		<div className={`flex flex-col h-full overflow-hidden py-4 px-6 ${className}`}>
+		<div className={`flex flex-col h-full overflow-hidden px-6 ${className}`}>
 			{missingShippingCount > 0 && (
 				<div className="bg-yellow-50 border-l-4 border-yellow-400 p-4 mb-4">
 					<div className="flex">
--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -71,8 +71,8 @@ function SheetContent({
         {...props}
       >
         {children}
-        <SheetPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none">
-          <XIcon className="size-8" />
+        <SheetPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none w-6 h-6 flex items-center justify-center">
+          <XIcon className="size-6" />
           <span className="sr-only">Close</span>
         </SheetPrimitive.Close>
       </SheetPrimitive.Content>
