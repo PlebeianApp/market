ba198dfe18b4530e3da1cf5b0db4516b4d961fc4
zazawowow <zazawowow@icloud.com>
2025-08-07 05:59:21 -0500

    fix: banner top gap issue but still not completely fixed

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -54,9 +54,9 @@ function Index() {
 				</div>
 
 				<div className="hero-content">
-					<div className="flex flex-col items-center justify-center text-white text-center lg:col-span-2 relative z-20 mt-16 lg:mt-0 gap-4 lg:gap-0">
+					<div className="flex flex-col items-center justify-center text-white text-center lg:col-span-2 relative z-20 mt-16 lg:mt-0">
 						<div className="flex items-center justify-center h-24 lg:h-32">
-							<h1 className="text-3xl lg:text-5xl font-theylive transition-opacity duration-500">Buy & Sell Stuff with sats</h1>
+							<h1 className="text-4xl lg:text-5xl font-theylive transition-opacity duration-500">Buy & Sell Stuff with sats</h1>
 						</div>
 
 						<div className="flex flex-col gap-6">
