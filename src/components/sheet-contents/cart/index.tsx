856dc00f670bae8441d7e9c1669f553eafd414c3
hkarani <hezronkaranii@gmail.com>
2025-09-27 17:48:06 +0300

    fix: cart animation-out on close

--- src/components/sheet-contents/cart/index.tsx
+++ src/components/sheet-contents/cart/index.tsx
@@ -1,6 +1,8 @@
-import { SheetContent, SheetDescription, SheetHeader, SheetTitle } from '@/components/ui/sheet'
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from '@/components/ui/sheet'
 import { cartStore } from '@/lib/stores/cart'
+import { uiStore, uiActions } from '@/lib/stores/ui'
 import { useStore } from '@tanstack/react-store'
+import { useEffect, useState } from 'react'
 import { CartContent } from './CartContent'
 import { EmptyCartScreen } from './EmptyCartScreen'
 
@@ -12,24 +14,44 @@ export default function CartSheetContent({
 	description?: string
 }) {
 	const { cart } = useStore(cartStore)
+	const { drawers } = useStore(uiStore)
 	const isCartEmpty = Object.keys(cart.products).length === 0
+	const [open, setOpen] = useState(drawers.cart)
+
+	useEffect(() => {
+		setOpen(drawers.cart)
+	}, [drawers.cart])
+
+	const handleOpenChange = (nextOpen: boolean) => {
+		setOpen(nextOpen)
+		if (!nextOpen) {
+			// Delay closing in store until after animation
+			setTimeout(() => {
+				uiActions.closeDrawer('cart')
+			}, 300) // matches slide-out duration
+		}
+	}
 
 	if (isCartEmpty) {
 		return (
-			<SheetContent side="right" className="flex flex-col max-h-screen w-[100vw] sm:w-[85vw] md:w-[55vw] xl:w-[35vw]">
-				<EmptyCartScreen />
-			</SheetContent>
+			<Sheet open={open} onOpenChange={handleOpenChange}>
+				<SheetContent side="right" className="flex flex-col max-h-screen w-[100vw] sm:w-[85vw] md:w-[55vw] xl:w-[35vw]">
+					<EmptyCartScreen />
+				</SheetContent>
+			</Sheet>
 		)
 	}
 
 	return (
-		<SheetContent side="right" className="flex flex-col max-h-screen w-[100vw] sm:w-[85vw] md:w-[55vw] xl:w-[35vw]">
-			<SheetHeader>
-				<SheetTitle>{title}</SheetTitle>
-				<SheetDescription className="hidden">{description}</SheetDescription>
-			</SheetHeader>
-			<CartContent />
-		</SheetContent>
+		<Sheet open={open} onOpenChange={handleOpenChange}>
+			<SheetContent side="right" className="flex flex-col max-h-screen w-[100vw] sm:w-[85vw] md:w-[55vw] xl:w-[35vw]">
+				<SheetHeader>
+					<SheetTitle>{title}</SheetTitle>
+					<SheetDescription className="hidden">{description}</SheetDescription>
+				</SheetHeader>
+				<CartContent />
+			</SheetContent>
+		</Sheet>
 	)
 }
 
