e03cd5dd7c0c886c1e67e8718bbe1049180d4a89
plebobmoto <plebomoto@proton.me>
2025-10-22 11:59:37 +0200

    feat: add toaster

--- src/components/sheet-contents/products/tabs.tsx
+++ src/components/sheet-contents/products/tabs.tsx
@@ -4,18 +4,17 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
-import { Spinner } from '@/components/ui/spinner'
 import { CURRENCIES, PRODUCT_CATEGORIES } from '@/lib/constants'
 import type { RichShippingInfo } from '@/lib/stores/cart'
 import { useNDK } from '@/lib/stores/ndk'
 import { productFormActions, productFormStore, type ProductShippingForm } from '@/lib/stores/product'
-import { uiStore, useUI } from '@/lib/stores/ui'
+import { uiStore } from '@/lib/stores/ui'
 import { MempoolService } from '@/lib/utils/mempool'
 import { useBtcExchangeRates, type SupportedCurrency } from '@/queries/external'
 import { createShippingReference, getShippingInfo, useShippingOptionsByPubkey } from '@/queries/shipping'
 import { useForm } from '@tanstack/react-form'
 import { useStore } from '@tanstack/react-store'
-import { CheckIcon, PackageIcon, PlusIcon, SettingsIcon, TruckIcon, X, Loader2, Info } from 'lucide-react'
+import { Info, Loader2, PackageIcon, PlusIcon, TruckIcon, X } from 'lucide-react'
 import { useEffect, useMemo, useState } from 'react'
 import { toast } from 'sonner'
 
@@ -370,7 +369,10 @@ export function CategoryTab() {
 	}
 
 	const addSubCategory = () => {
-		if (categories.length >= 3) return // Prevent adding more than 3
+			if (categories.length >= 3)  {
+			toast.error('You can only add up to 3 sub categories')
+			return
+		}
 		productFormActions.updateCategories([
 			...categories,
 			{
