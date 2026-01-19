e389ff0c0510054ac04fade950d70b8be7d2f140
plebobmoto <plebomoto@proton.me>
2026-01-19 16:26:27 +0100

    style: remove space in empty arrow function return.

--- src/lib/stores/ndk.ts
+++ src/lib/stores/ndk.ts
@@ -455,7 +455,7 @@ export const ndkActions = {
 		const state = ndkStore.state
 		if (!state.zapNdk || !state.isZapNdkConnected) {
 			console.warn('Zap NDK not connected. Cannot create zap subscription.')
-			return () => { }
+			return () => {}
 		}
 
 		const filters: any = {
